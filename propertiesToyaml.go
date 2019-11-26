8162b6bc7e9d34df9bab47021c713977ad64180e
夏至未至 <994562751@qq.com>
2019-11-26 20:50:42 +0800

    删除了下无用的逻辑以及加了下注释方便大家理解

--- propertiesToyaml.go
+++ propertiesToyaml.go
@@ -25,14 +25,7 @@ func main() {
 			//每次新加入一行都需要进行调整已经生成的结果
 			updateMap(resmap,tempmap)
 		}
-	//	c,_:=json.Marshal(resmap)
-	//	fmt.Println(string(c))
-		//ss:=map[string]interface{}{}
-
 		processlist(&resmap)
-
-	//	cc,_:=json.Marshal(&resmap)
-	//	fmt.Println(string(cc))
 		d, _ := yaml.Marshal(&resmap)
 		fmt.Println(string(d))
 		ioutil.WriteFile("/Users/hq/go/src/gotest/src/company/test.yaml",d,os.ModePerm)
@@ -41,62 +34,64 @@ func main() {
 
 func updateMap( omap map[string]interface{},desmap map[string]interface{}) map[string]interface{} {
 	for k,v:= range desmap {
-
 		//如果是map说明没有递归完成，继续完成递归
 		if t(v) {
 			x,ok:=omap[k]
 			if ok {
 			omap[k] = updateMap(x.(map[string]interface{}),v.(map[string]interface{}))
 			} else {
-			omap[k] = 	updateMap(map[string]interface{}{},v.(map[string]interface{}))
+			omap[k] = updateMap(map[string]interface{}{},v.(map[string]interface{}))
 			}
 		}else {//终止条件
-					//后者覆盖前者.
-					omap[k]=desmap[k]
-
-				}
-			}
-			return omap
+		//后者覆盖前者.
+		omap[k]=desmap[k]
 		}
-
+	}
+	return omap
+}
+//数组 这里可以不用 指针的方式，纯粹是为了装b
 func processlist(resMap *map[string]interface{}) {
 	var count int = len(*resMap)
 	for k,v:= range *resMap {
+		//判断是否key包含了数组的符号
 		if strings.Contains(k,"[",) && strings.Contains(k,"]") && (strings.LastIndex(k,"]") + 1== len(k)) {
 			x, ok := (*resMap)[string([]rune(k)[:strings.LastIndex(k,"[")])]
+			//已经存在
 			if ok {
+				//如果是v是map,处理它的孩子并把它加到数组里面
 				if t(v) {
 					xxx:=v.(map[string]interface{})
 					processlist(&xxx)
 				}
 				x = append(x.([]interface{}), v)
+				//创建一个数组子项，给后面生成yaml使用的数组
 				(*resMap)[string([]rune(k)[:strings.LastIndex(k,"[")])] = x
 				delete(*resMap, k)
 				count--;
+				//key已经全部遍历完成
 				if count==0 {
 					return
 				}
+				//该项处理完成 继续处理下一项
 				continue
-
 			} else {
 				if t(v) {
 					xxx:=v.(map[string]interface{})
 					processlist(&xxx)
 				}
+				//首次
 				x = append([]interface{}{}, v)
 				(*resMap)[string([]rune(k)[:strings.LastIndex(k,"[")])] = x
 				delete(*resMap, k)
 				count--;
+				//遍历完成
 				if count==0 {
 					return
 				}
 				continue
 			}
 		}
-
-		if st(resMap) {
-			return
-		}
+		//如果是字符串说明已经到达最后直接下次循环
 		if st(v)  {
 			count--;
 			if count<=0 {
@@ -104,18 +99,17 @@ func processlist(resMap *map[string]interface{}) {
 			}
 		continue
 		}
+		//因为加入了数组所以这里遍历到需要把它跳过
 		if intert(v) {
 			continue
 		}
+		//正常的情况不带数组标记的
 		x:=v.(map[string]interface{})
 			processlist(&x)
-		count--;
-		if count ==0 {
+		if  0 == count--  {
 			return
 		}
 	}
-
-
 }
 
 func intert(i interface{})bool {    //函数t 有一个参数i
@@ -150,6 +144,7 @@ func it(i interface{})bool {    //函数t 有一个参数i
 	}
 	return false
 }
+//文件处理
 func getpropertiesMap(in string) map[string]string  {
 	content,err:= ioutil.ReadFile( in)
 	if err != nil {
